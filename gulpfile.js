cd545325249b98ca165ae7eae5d08bf04faa03df
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-07-21 13:24:39 -0500

    CSC 2018/07/21: Conociendo gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,15 @@ autoprefixer = require('gulp-autoprefixer');
 gulp.task('sass', () => {
    gulp.src('scss/app.scss')
      .pipe(autoprefixer({
-
+         browsers: ['last 2 versions'], cascade: false
      }))
      .pipe(sass({
          includePaths: ['scss']
      }))
-     .pipe(gulp.dest('css'))
+     .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', ['sass'], () => {
+    //todos los archivos con extensión .scss
+  gulp.watch(['scss/*.scss'], ['sass']);
 });
