489d05cb3e51b14d9729ac1ae116bc539e2b8f60
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-07-21 13:08:47 -0500

    CSC 2018/07/21: Creando trareas con gulp
    Se creo una tarea con 3 pipes

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp'),
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', () => {
+   gulp.src('scss/app.scss')
+     .pipe(autoprefixer({
+
+     }))
+     .pipe(sass({
+         includePaths: ['scss']
+     }))
+     .pipe(gulp.dest('css'))
+});
