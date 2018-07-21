5ca0955e07c31c1adb04e532b70ba2fd2e0cfc13
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-07-21 18:19:19 -0500

    CSC 2018/07/21: Probando la libreria bxSlider

---
+++ bxSliderMain.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('.popular').bxSlider({
+      auto: true,
+      autoControls: true,
+      speed: 500,
+      slideSelector: 'div.item',
+      minSlides: 5,
+      maxSlides: 5,
+      moveSlides: 5,
+      slideWidth: 200
+      /*,
+      slideMargin: 30*/
+    });
+  });
